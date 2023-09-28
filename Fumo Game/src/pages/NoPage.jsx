7525946ff162f92d96c8b1401b2480a728e4ecb3
maxfuballers <94360196+Michel-Ranok@users.noreply.github.com>
2023-09-28 14:22:59 +0200

    Fumo Game First commit

---
+++ Fumo Game/src/pages/NoPage.jsx
@@ -0,0 +1,12 @@
+function NoPage() {
+    return (
+        <>
+            <div className="nopage">
+                <h1>404</h1>
+                <p>tu t'es perdu mec</p>
+            </div>
+        </>
+    )
+}
+
+export default NoPage;
